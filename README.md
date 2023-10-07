bfc5229f6343d25514fe51562f3e46487f065b3a
hiddenccash <hiddenccash@gmail.com>
2023-10-07 01:13:08 -0700

    finish

---
+++ README.md
@@ -0,0 +1,66 @@
+# Model-Predictive-Control
+
+![gif](https://github.com/JLee21/Model-Predictive-Control/blob/master/img/gif.gif)
+
+## The Overall Model of the Vehicle
+
+The vehicle runs within a 3D Unity program that resembles life-like driving behavior.
+The *state* of the Model consists of the following:
+
+State Variables | Description
+-------------- | -----------
+x | the x position of the car in global map coordinates measured in meters
+y | the y position of the car in global map coordinates measured in meters
+psi | the orientation of the car in relation to the global map coordinate
+v | the current speed of the car measured in miles per hour
+
+Actuator Variables | Descriptions
+-------------- | -----------
+accel | the value of the acceleration in meters per second squared. In this case, think of `1` being full throttle and `-1` being full brake.
+delta | the steering angle value in radians that is sent as the steering command
+
+The above variables are known at every single timestep of the program.
+
+### Update Equations
+
+Hence the `Predictive` in Model-Predictive-Control, the model's state is updated using the following kinematic equations.
+
+![update-eqn](https://github.com/JLee21/Model-Predictive-Control/blob/master/img/update-eqn.JPG)
+
+Taking into account everything mentioned above, the model's state can be predicted several time steps into the future.
+But how can we decide what Accelerator commands (`accel` value) and Steering commands (`delta` value) to send to the model?
+Both of these commands affect how the model behaves as discuessed below.
+
+## Calculating the Cost of the Model's Prediction
+
+Note only is the model's state known at each time step, but also location markers in the middle of the track known as Way Points. At any given time step, the immediately next 6 Way Points are known. Since the Way Points lie in the middle of the track and evenly spaced, a third-order polynomial can be fitted to these six points. The resulting polynomial, or rather coefficients of the polynomial, represent the reference trajectory for the model as this fitted trajectory aligns closely with the middle of the track's path. Now, not only is the ideal/reference path known but also the predicted path of the model. As discussed earlier, the driving commands (steering angle, accelerator) are optimized to fit the predicted path as closely to the ideal path as possible. Below are the equations used to calculate the cost of the model from the ideal path.
+
+![cost-eqn](https://github.com/JLee21/Model-Predictive-Control/blob/master/img/cost-eqn.JPG)
+
+1) `cte` is the Cross Track Error of the model as defined as the approximate distance from the model's center to the center of the track. `coeffs` is the coefficients of the fitted third-order polynomial and `px` is the x position of the model (here, in relation to the car's reference). The function `polyeval` computes the y value at position `x`. This y values signifies how much lateral distance between the model and the track's center.
+2) `depsi` is the desired heading (in radians) of the model. This value is calculated by taking the inverse tangent of the reference trajectory at position `px`. Now, `epsi`, or the error of the model's current heading is calculated as the difference between the desired heading and the current heading.
+
+## How Much To Predict in the Future
+
+The model's predicted state becomes less accurate the further it is predicted into the future.
+Also, the model's predicted state is only calculated at discrete points in time. The variables `N` and `dt` address how far into the future the model is predicated. `dt` is the length of time in between each calculated state. `N` is the number of discrete points in time to calculate the model's state. Now the total time into the future is known as `N * dt`. The final chosen `dt` was empirically found to be 200 milliseconds. A value too low would result in unnoticeable improvement at the cost of extra computation. With a value too high, the model would often appear sluggish in its reaction to turns causing the model to overreact/overcompensate with a stark steering command.
+Similarly, the `N` value, a high value showed no added handling performance as the accuracy of the model's predicted state dimenished further in time. Too low of a value the model may not 'see' several timesteps into the next sharp turn. A value of `N` is 10 was chosen.
+
+## Order of Operations
+
+To quickly summarize the Order of Operations, the model receives not only the state of itself (x, y positions, etc.) but also the immediately next six Way Points. The Way Points are then used to calculate the reference, or ideal, trajectory of the model (in actuality, the referenced trajectory is the coefficients to a fitted third-order polynomial). Next, the state of the model is predicted one time step into the future in order to compensate for the simulated latency that is common in real driving conditions (discussed in detail later). Now that the model's (latency-incorporated) state and reference trajectory is known the Interior Point OPTimizer (IPOPT) software package solver is able to calculate the best driving commands so as to emulate the reference trajectory.
+
+## Incorporating Simulated Latency
+
+In real driving conditions, when a driving command such as steering is issued by a drive-by-wire system there is inherent lag in time until the command is fully realized by the vehicle's component. In other words, the steering wheel may not actually achieve the commanded steering wheel angle until a fraction of a second into the future. As briefly mentioned earlier, latency compensation is achieved by predicting the model's state into the future by the same amount of latency time (in this case, 100 milliseconds). As a reminder, the model's state is sent to the IPOPT solver and the solver treats this state as the actual current state of the model even though the state is a predicted state 100ms into the future. Below are the equations used to update the model's state to compensate for latency:
+
+```
+v *= 0.44704                          (1)
+v += accel * LATENCY_SEC              (2)
+psi = -v * delta / Lf * LATENCY_SEC   (3)
+px = v * LATENCY_SEC                  (4)
+```
+1) The velocity `v` in miles per hour is first converted to meters per second
+2) Here, the predicted velocity is approximated to change only in the forward direction of the car (x-direction). Given a small time step into the future, `LATENCY_SEC`, and the current acceleration of the model, `accel`, the predicted velocity is calculated.
+3) `psi` is the bearing of the car in radians. `delta` is the current steering wheel command in radians and Lf is a constant related to the model's turning radius at constant velocity.
+4) Similar to the predicted velocity, the model's x position, `px`, is approximated to be entirely in the forward direction (x-direction). In addition, the model's y position is not calculated since the model does not shift laterally to any noticeable degree.
---
+++ src/main.cpp
@@ -0,0 +1,225 @@
+#include <math.h>
+#include <uWS/uWS.h>
+#include <chrono>
+#include <iostream>
+#include <stdio.h>
+#include <thread>
+#include <vector>
+#include "Eigen-3.3/Eigen/Core"
+#include "Eigen-3.3/Eigen/QR"
+#include "MPC.h"
+#include "json.hpp"
+
+// for convenience
+using json = nlohmann::json;
+using Eigen::VectorXd;
+
+// For converting back and forth between radians and degrees.
+constexpr double pi() { return M_PI; }
+double deg2rad(double x) { return x * pi() / 180; }
+double rad2deg(double x) { return x * 180 / pi(); }
+const int MAXSTEER_DEG = 25;
+const double LATENCY_SEC = 0.1; // in seconds
+const double Lf = 2.67;
+
+// Checks if the SocketIO event has JSON data.
+// If there is data the JSON object in string format will be returned,
+// else the empty string "" will be returned.
+string hasData(string s) {
+  auto found_null = s.find("null");
+  auto b1 = s.find_first_of("[");
+  auto b2 = s.rfind("}]");
+  if (found_null != string::npos) {
+    return "";
+  } else if (b1 != string::npos && b2 != string::npos) {
+    return s.substr(b1, b2 - b1 + 2);
+  }
+  return "";
+}
+
+// Evaluate a polynomial.
+double polyeval(Eigen::VectorXd coeffs, double x) {
+  double result = 0.0;
+  for (int i = 0; i < coeffs.size(); i++) {
+    result += coeffs[i] * pow(x, i);
+  }
+  return result;
+}
+
+// Fit a polynomial. adapted from http://bit.ly/2hAf75b
+Eigen::VectorXd polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals,
+                        int order) {
+  assert(xvals.size() == yvals.size());
+  assert(order >= 1 && order <= xvals.size() - 1);
+  Eigen::MatrixXd A(xvals.size(), order + 1);
+
+  for (int i = 0; i < xvals.size(); i++) {
+    A(i, 0) = 1.0;
+  }
+
+  for (int j = 0; j < xvals.size(); j++) {
+    for (int i = 0; i < order; i++) {
+      A(j, i + 1) = A(j, i) * xvals(j);
+    }
+  }
+
+  auto Q = A.householderQr();
+  auto result = Q.solve(yvals);
+  return result;
+}
+
+int main() {
+  uWS::Hub h;
+
+  // MPC is initialized here!
+  MPC mpc;
+
+  h.onMessage([&mpc](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+                     uWS::OpCode opCode) {
+    // "42" at the start of the message means there's a websocket message event.
+    // The 4 signifies a websocket message
+    // The 2 signifies a websocket event
+    string sdata = string(data).substr(0, length);
+    // cout << sdata << endl;
+    if (sdata.size() > 2 && sdata[0] == '4' && sdata[1] == '2') {
+      string s = hasData(sdata);
+      if (s != "") {
+        auto j = json::parse(s);
+        string event = j[0].get<string>();
+        if (event == "telemetry") {
+          // j[1] is the data JSON object
+          // six waypoints are given at a time
+          vector<double> ptsx = j[1]["ptsx"];
+          vector<double> ptsy = j[1]["ptsy"];
+          double px = j[1]["x"];
+          double py = j[1]["y"];
+          double psi = j[1]["psi"];
+          double v = j[1]["speed"];
+          double delta = j[1]["steering_angle"];
+          double accel = j[1]["throttle"];
+
+          // transform waypoint coords from global to vehicle
+          Eigen::VectorXd ptsx_trans(ptsx.size());
+          Eigen::VectorXd ptsy_trans(ptsy.size());
+          for (int i = 0; i < ptsx.size(); i++) {
+            ptsx_trans(i) = (ptsx[i]-px)*cos(0-psi) - (ptsy[i]-py)*sin(0-psi);
+            ptsy_trans(i) = (ptsy[i]-py)*cos(0-psi) + (ptsx[i]-px)*sin(0-psi);
+          }
+
+          // fit a third order polynomial to the 6 given waypoints
+          Eigen::VectorXd coeffs = polyfit(ptsx_trans, ptsy_trans, 3);
+
+          /*
+          compute errors of the model
+          the following assumes that px=py=0 since the model's position and yaw
+          is 'tared'
+           */
+          double cte = polyeval(coeffs, 0);
+          double epsi = -atan(coeffs[1]);
+
+          // compute model predictions for the sake of latency
+          v *= 0.44704; // convert from miles/hour to meters/sec
+          px = v * LATENCY_SEC;
+          py = 0; // assume zero or very little lateral movement
+          psi = -v * delta * LATENCY_SEC / Lf; // `-` b/c str cmd is flipped in Unity
+          epsi += psi;
+          cte += v * sin(epsi) * LATENCY_SEC;
+          v += accel * LATENCY_SEC;
+
+          Eigen::VectorXd state(6);
+          state << px, py , psi, v, cte, epsi;
+
+          // compute the ideal driving commands using IPOPT
+          auto vars = mpc.Solve(state, coeffs);
+
+          // Calculate steering angle and throttle using MPC.
+          // Both are between [-1, 1].
+          double steer_value;
+          double throttle_value;
+          // NOTE: Remember to divide by deg2rad(25) before
+          // you send the steering value back.
+          steer_value = vars[0] / deg2rad(MAXSTEER_DEG);
+          throttle_value = vars[1];
+
+          //Display the MPC predicted trajectory
+          vector<double> mpc_x_vals;
+          vector<double> mpc_y_vals;
+          for (int i = 3; i < vars.size(); i++){
+            if ( i % 2 == 0 ){
+              mpc_x_vals.push_back(vars[i]);
+            }
+            else {
+              mpc_y_vals.push_back(vars[i]);
+            }
+          }
+
+          // Display the waypoints/reference line
+          // the points in the simulator are connected by a Yellow line
+          vector<double> next_x_vals;
+          vector<double> next_y_vals;
+          double poly_inc = 2.5;
+          int num_points = 25;
+          for (int i = 0; i < num_points; i++) {
+            next_x_vals.push_back( poly_inc * i );
+            next_y_vals.push_back( polyeval(coeffs, poly_inc * i) );
+          }
+
+          json msgJson;
+          msgJson["steering_angle"] = steer_value;
+          msgJson["throttle"] = throttle_value;
+
+          msgJson["mpc_x"] = mpc_x_vals;
+          msgJson["mpc_y"] = mpc_y_vals;
+
+          msgJson["next_x"] = next_x_vals;
+          msgJson["next_y"] = next_y_vals;
+
+          auto msg = "42[\"steer\"," + msgJson.dump() + "]";
+
+          // Latency
+          // The purpose is to mimic real driving conditions where
+          // the car does not actuate the commands instantly.
+          this_thread::sleep_for(chrono::milliseconds(100));
+
+          ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+        }
+      } else {
+        // Manual driving
+        std::string msg = "42[\"manual\",{}]";
+        ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+      }
+    }
+  });
+
+  // We don't need this since we're not using HTTP but if it's removed the
+  // program doesn't compile :-(
+  h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data,
+                     size_t, size_t) {
+    const std::string s = "<h1>Hello world!</h1>";
+    if (req.getUrl().valueLength == 1) {
+      res->end(s.data(), s.length());
+    } else {
+      // i guess this should be done more gracefully?
+      res->end(nullptr, 0);
+    }
+  });
+
+  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
+    std::cout << "Connected!!!" << std::endl;
+  });
+
+  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
+                         char *message, size_t length) {
+    ws.close();
+    std::cout << "Disconnected" << std::endl;
+  });
+
+  int port = 4567;
+  if (h.listen(port)) {
+    std::cout << "Listening to port " << port << std::endl;
+  } else {
+    std::cerr << "Failed to listen to port" << std::endl;
+    return -1;
+  }
+  h.run();
+}
